f8297a4ade73d5ba7c6405e2232d18e0f50445ae
ClearlyOllie <95652649+ClearlyOllie@users.noreply.github.com>
2022-08-09 19:30:44 -0500

    Update monopoly.py

--- monopoly.py
+++ monopoly.py
@@ -1,11 +1,26 @@
-#in development
+# IN DEVELOPMENT NOT COMPLETE!
+#credit to ollie#1459 (discord)
+import os, random, time #python module imports
 
-import time, random, os
+class main(): #main class nesting the game
 
-class main():
-class vars():
-waitTime = 0
-minPlayers = 2
-maxPlayers = 6
-def game(wTime):
-main.game()
+    class players(): #controls player values & adding new players
+        playersTb = []
+        def __init__(self, name, cash, position):
+            self.name = name
+            self.cash = cash
+            self.position = position
+   
+    class vars(): #class nesting game variables - can be edited
+        waitTime = 0
+        minPlayers = 2
+        maxPlayers = 6
+        
+    def game(wTime, minPlayers, maxPlayers):
+        print('Welcome to Monopoly in Python [TERMINAL EDITION]!')
+        time.sleep(wTime)
+        startOption = input('1. Add new player/n2. Start game/n/nEnter your option: ')
+        #match startOption:
+
+main.game(game.vars.waitTime) #run the game
+
